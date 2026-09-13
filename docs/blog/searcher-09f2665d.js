998297847ac325b386750c8b4058f67527957f49
Pramod Thaz <pramodty@gmail.com>
2026-09-13 15:25:48 -0500

    Publish blog — 2026-09-13 15:25

--- docs/blog/searcher-09f2665d.js
+++ docs/blog/searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-ef1e6dbf.js',
+                path_to_root + 'searchindex-0b05a119.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
